5e0afec11f903d0a90a33f02c6c1a0492ba26671
Basmola.innovation <Basmola.basmalaamr9@gmail.com>
2025-12-03 15:40:52 +0200

    Initial commit

---
+++ BtightRoute/brightroute/src/test/java/com/brightroute/brightroute/BrightrouteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brightroute.brightroute;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrightrouteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
