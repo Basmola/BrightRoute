81681319c00b1ca17e68d38153a9650a59df3b56
Salma Amr <salmaamr1520@gmail.com.>
2025-12-11 14:34:59 +0200

    save my local changes

---
+++ brightroute/src/main/java/com/brightroute/brightroute/repository/StudentRepository.java
@@ -0,0 +1,10 @@
+package com.brightroute.brightroute.repository;
+
+public class StudentRepository {
+
+    public Object findById(Long studentId) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'findById'");
+    }
+
+}
