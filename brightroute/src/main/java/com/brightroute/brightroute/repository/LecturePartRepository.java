c7e0713907ba3808fdda9bc0d24e1b92da8b8339
Ramez-Markus <ramezmarkus1234@gmail.com>
2025-12-12 17:50:13 +0200

    fix apis users and lectures and courses and lectures-parts

--- brightroute/src/main/java/com/brightroute/brightroute/repository/LecturePartRepository.java
+++ brightroute/src/main/java/com/brightroute/brightroute/repository/LecturePartRepository.java
@@ -3,4 +3,6 @@ package com.brightroute.brightroute.repository;
 import com.brightroute.brightroute.model.LecturePart;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface LecturePartRepository extends JpaRepository<LecturePart, Integer> {}
+public interface LecturePartRepository extends JpaRepository<LecturePart, Integer> {
+    // Basic CRUD operations are inherited from JpaRepository
+}
