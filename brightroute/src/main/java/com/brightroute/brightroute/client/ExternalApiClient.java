2d97f69c8c457ed625e2fff4ad9bbf9645b2e59d
Ramez-Markus <ramezmarkus1234@gmail.com>
2025-12-14 23:55:42 +0200

    lasssssst finallllll comittttttttttttttttttttttttttt

--- brightroute/src/main/java/com/brightroute/brightroute/client/ExternalApiClient.java
+++ brightroute/src/main/java/com/brightroute/brightroute/client/ExternalApiClient.java
@@ -3,8 +3,6 @@ package com.brightroute.brightroute.client;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
-// This component uses Spring Cloud OpenFeign to talk to an internal service (Loopback)
-// This avoids external network restrictions/errors while still using the Spring Cloud technology.
 @FeignClient(name = "localClient", url = "http://localhost:7070")
 public interface ExternalApiClient {
 
